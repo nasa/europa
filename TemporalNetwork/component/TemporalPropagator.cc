d44ea503e9d3e10359b4f8b61a171b16359e7b1f
Sailesh Ramakrishnan <no_author@no_author.com>
2004-02-16 20:15:35 +0000

    propagator for tn
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@1332 23e1bc29-d954-0410-9766-47de34158ba0

---
+++ TemporalNetwork/component/TemporalPropagator.cc
@@ -0,0 +1,203 @@
+#include "TemporalPropagator.hh"
+#include "TemporalNetworkDefs.hh"
+#include "TokenTemporalVariable.hh"
+#include "Token.hh"
+#include "TemporalNetwork.hh"
+#include "ConstraintEngine.hh"
+#include "ConstrainedVariable.hh"
+#include "Constraint.hh"
+
+namespace Prototype {
+
+  TemporalPropagator::TemporalPropagator(const LabelStr& name, const ConstraintEngineId& constraintEngine)
+    : Propagator(name, constraintEngine), 
+      m_activeConstraint(0),
+      m_updateRequired(false){
+    m_tnet = (new TemporalNetwork())->getId();
+  }
+
+  TemporalPropagator::~TemporalPropagator() {
+    delete (TemporalNetwork*) m_tnet;
+  }
+
+  void TemporalPropagator::handleConstraintAdded(const ConstraintId& constraint){
+    handleTemporalAddition(constraint);
+  }
+
+  void TemporalPropagator::handleConstraintRemoved(const ConstraintId& constraint){
+    handleTemporalDeletion(constraint);
+  }
+
+  void TemporalPropagator::handleConstraintActivated(const ConstraintId& constraint){
+    handleTemporalAddition(constraint);
+  }
+
+  void TemporalPropagator::handleConstraintDeactivated(const ConstraintId& constraint){
+    handleTemporalDeletion(constraint);
+  }
+
+  void TemporalPropagator::handleNotification(const ConstrainedVariableId& variable, 
+					     int argIndex, 
+					     const ConstraintId& constraint, 
+					     const DomainListener::ChangeType& changeType){
+    if ((constraint->getKey() != m_activeConstraint) &&
+	(constraint->getName() == LabelStr("StartEndDurationRelation")))
+      m_agenda.insert(constraint);
+    m_updateRequired = true;
+  }
+
+  void TemporalPropagator::execute(){
+    check_error(!getConstraintEngine()->provenInconsistent());
+    check_error(m_activeConstraint == 0);
+
+    //update the tnet
+    updateTnet();
+    //propagate the tnet
+    m_tnet->isConsistent();
+    //update the cnet
+    updateTempVar();
+
+    while(!m_agenda.empty()){
+      std::set<ConstraintId>::iterator it = m_agenda.begin();
+      ConstraintId constraint = *it;
+
+      if(constraint->isActive()){
+	m_activeConstraint = constraint->getKey();
+	Propagator::execute(constraint);
+      }
+
+      if(getConstraintEngine()->provenInconsistent()){
+	m_agenda.clear();
+	break;
+      }
+      else
+	m_agenda.erase(it);
+    }
+    m_activeConstraint = 0;
+    m_updateRequired = false;
+  }
+
+  bool TemporalPropagator::updateRequired() const{
+    return (m_updateRequired);
+  }
+
+  void TemporalPropagator::checkAndAddTnetVariables(const ConstraintId& constraint) {
+
+    std::vector<TempVarId> vars;
+
+    if (constraint->getName() == LabelStr("StartEndDurationRelation")) {    
+      vars.push_back(constraint->getScope()[0]);  
+      vars.push_back(constraint->getScope()[2]); //skip the duration variable
+    }
+    else {
+      vars.push_back(constraint->getScope()[0]);  
+      vars.push_back(constraint->getScope()[1]);      
+    }
+    //add the variables to the tnet if not there already
+    for(std::vector<TempVarId>::const_iterator it = vars.begin(); it != vars.end(); ++it) {
+      check_error(TempVarId::convertable((*it)));
+      TempVarId var = (*it);
+      if (m_tnetVariables.find(var->getKey()) == m_tnetVariables.end()) {
+	TimepointId timepoint = m_tnet->addTimepoint(var);
+	var->setTnetVariable(timepoint);
+	m_tnetVariables.insert(std::make_pair(var->getKey(), timepoint));
+	TemporalConstraintId c = m_tnet->addTemporalConstraint(m_tnet->getOrigin(), 
+							       timepoint, 
+							       (Time) var->getBaseDomain().getLowerBound(),  
+							       (Time) var->getBaseDomain().getUpperBound());
+	m_tnetVariableConstraints.insert(std::make_pair(var->getKey(), c));
+      }
+    }
+  }
+
+  void TemporalPropagator::addTnetConstraint(const ConstraintId& constraint) {
+    TempVarId start;
+    TempVarId end;
+    Time lb, ub;
+
+    if (constraint->getName() == LabelStr("StartEndDurationRelation")) {    
+      start = constraint->getScope()[0];
+      TempVarId duration = constraint->getScope()[1];
+      end = constraint->getScope()[2];
+
+      check_error(getTimepoint(start) != TimepointId::noId());
+      check_error(getTimepoint(end) != TimepointId::noId());
+
+      lb = (Time) duration->getBaseDomain().getLowerBound();
+      ub = (Time) duration->getBaseDomain().getUpperBound();
+
+    }
+    else if (constraint->getName() == LabelStr("concurrent")) {
+      start = constraint->getScope()[0];
+      end = constraint->getScope()[1];      
+      lb = 0;
+      ub = 0;
+    }
+    else if (constraint->getName() == LabelStr("before")) {
+      start = constraint->getScope()[0];
+      end = constraint->getScope()[1];      
+      lb = 0;
+      ub = g_infiniteTime();
+    }
+    
+    TemporalConstraintId c = m_tnet->addTemporalConstraint(getTimepoint(start), 
+							   getTimepoint(end), 
+							   lb,  
+							   ub);
+    m_tnetConstraints.insert(std::make_pair(constraint->getKey(), c));
+
+  }
+
+  TimepointId TemporalPropagator::getTimepoint(const TempVarId& var) {
+    if ( m_tnetVariables.find(var->getKey())  == m_tnetVariables.end())
+      return TimepointId::noId();
+    else
+      return (* m_tnetVariables.find(var->getKey())).second;
+  }
+
+  void TemporalPropagator::handleTemporalAddition(const ConstraintId& constraint) {
+    checkAndAddTnetVariables(constraint);
+    addTnetConstraint(constraint);
+    m_updateRequired = true;
+    if (constraint->getName() == LabelStr("StartEndDurationRelation")) 
+      m_agenda.insert(constraint);    
+  }
+
+  void TemporalPropagator::handleTemporalDeletion(const ConstraintId& constraint) {
+    //buffer for deletion
+    m_constraintsForDeletion.insert(constraint->getKey());
+    // Remove from agenda
+    m_agenda.erase(constraint);
+  }
+
+  void TemporalPropagator::updateTnet() {
+
+    for(std::set<int>::const_iterator it = m_constraintsForDeletion.begin(); it != m_constraintsForDeletion.end(); ++it) {
+      check_error(m_tnetConstraints.find((*it)) != m_tnetConstraints.end());
+      m_tnet->removeTemporalConstraint((* m_tnetConstraints.find((*it))).second);
+      m_tnetConstraints.erase((*it));
+    }
+    m_constraintsForDeletion.clear();
+    
+    for(std::set<int>::const_iterator it = m_variablesForDeletion.begin(); it != m_variablesForDeletion.end(); ++it) {
+      check_error(m_tnetVariables.find((*it)) != m_tnetVariables.end());
+      m_tnet->deleteTimepoint((* m_tnetVariables.find((*it))).second);
+      m_tnet->removeTemporalConstraint((* m_tnetVariableConstraints.find((*it))).second);
+      m_tnetVariables.erase((*it));
+      m_tnetVariableConstraints.erase((*it));
+    }
+    m_variablesForDeletion.clear();    
+  }
+
+  void TemporalPropagator::updateTempVar() {
+    for(std::map<int, TimepointId>::const_iterator it = m_tnetVariables.begin(); it != m_tnetVariables.end(); ++it) {
+      Time lb, ub;
+      m_tnet->getTimepointBounds((*it).second, lb, ub);
+      
+      if (m_tnet->getVarIdFromTimepoint((*it).second)->lastDomain().getLowerBound() != lb ||
+      	  m_tnet->getVarIdFromTimepoint((*it).second)->lastDomain().getUpperBound() != ub) 
+      	m_tnet->getVarIdFromTimepoint((*it).second)->specify(IntervalIntDomain(lb, ub));
+    }
+  }
+    
+} //namespace
---
+++ TemporalNetwork/component/TemporalPropagator.hh
@@ -0,0 +1,53 @@
+#ifndef _H_TemporalPropagator
+#define _H_TemporalPropagator
+
+#include "Propagator.hh"
+#include "PlanDatabaseDefs.hh"
+#include "TemporalNetworkDefs.hh"
+#include <set>
+
+namespace Prototype {
+
+  class TemporalPropagator: public Propagator
+  {
+  public:
+    TemporalPropagator(const LabelStr& name, const ConstraintEngineId& constraintEngine);
+    virtual ~TemporalPropagator();
+    void execute();
+    bool updateRequired() const;
+  protected:
+    void handleConstraintAdded(const ConstraintId& constraint);
+    void handleConstraintRemoved(const ConstraintId& constraint);
+    void handleConstraintActivated(const ConstraintId& constraint);
+    void handleConstraintDeactivated(const ConstraintId& constraint);
+    void handleNotification(const ConstrainedVariableId& variable, 
+			    int argIndex, 
+			    const ConstraintId& constraint, 
+			    const DomainListener::ChangeType& changeType);
+
+  private:
+
+    void checkAndAddTnetVariables(const ConstraintId& constraint);
+    void addTnetConstraint(const ConstraintId& constraint);
+
+    void handleTemporalAddition(const ConstraintId& constraint);
+    void handleTemporalDeletion(const ConstraintId& constraint);
+
+    void updateTnet();
+    void updateTempVar();
+
+    TimepointId getTimepoint(const TempVarId& var);
+
+    std::set<ConstraintId> m_agenda;
+    int m_activeConstraint;
+
+    bool m_updateRequired;
+    TemporalNetworkId m_tnet;
+    std::map<int, TemporalConstraintId> m_tnetConstraints;
+    std::map<int, TemporalConstraintId> m_tnetVariableConstraints;
+    std::map<int, TimepointId> m_tnetVariables;
+    std::set<int> m_constraintsForDeletion; //!< Buffer deletions till you have to propagate.
+    std::set<int> m_variablesForDeletion;  //!< Buffer deletions till you have to propagate.
+  };
+}
+#endif
