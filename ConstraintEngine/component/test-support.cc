22d80cfd5c26553ce72555c2ecfedc480f1dbbc7
Will Edgington <no_author@no_author.com>
2004-03-19 23:32:42 +0000

    Add new CountZerosConstraint, CountNonZerosConstraint, and CardinalityConstraint.  The last was in NewPlan; the others are used by its implementation here.  All are considerably more general than the "card" constraint in NewPlan but the last is compatible with it.
    Alphabetize the calls to REGISTER_NARY() by the constraint names.
    
    
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@1501 23e1bc29-d954-0410-9766-47de34158ba0

--- ConstraintEngine/component/test-support.cc
+++ ConstraintEngine/component/test-support.cc
@@ -10,27 +10,33 @@ void initConstraintLibrary() {
   if (!s_runAlready) {
     // Register constraint Factories
     REGISTER_UNARY(SubsetOfConstraint, "SubsetOf", "Default");
-    REGISTER_NARY(EqualConstraint, "Equal", "Default");
     REGISTER_NARY(AddEqualConstraint, "AddEqual", "Default");
-    REGISTER_NARY(LessThanEqualConstraint, "LessThanEqual", "Default");
-    REGISTER_NARY(NotEqualConstraint, "NotEqual", "Default");
-    REGISTER_NARY(LessThanConstraint, "LessThan", "Default");
-    REGISTER_NARY(LessThanSumConstraint, "LessThanSum", "Default");
-    REGISTER_NARY(MultEqualConstraint, "MultEqual", "Default");
     REGISTER_NARY(AddMultEqualConstraint, "AddMultEqual", "Default");
-    REGISTER_NARY(EqualSumConstraint, "EqualSum", "Default");
-    REGISTER_NARY(EqualProductConstraint, "EqualProduct", "Default");
-    REGISTER_NARY(LessOrEqThanSumConstraint, "LessOrEqThanSum", "Default");
-    REGISTER_NARY(CondAllSameConstraint, "CondAllSame", "Default");
+    REGISTER_NARY(CardinalityConstraint, "Cardinality", "Default");
     REGISTER_NARY(CondAllDiffConstraint, "CondAllDiff", "Default");
+    REGISTER_NARY(CondAllSameConstraint, "CondAllSame", "Default");
+    REGISTER_NARY(CountNonZerosConstraint, "CountNonZeros", "Default");
+    REGISTER_NARY(CountZerosConstraint, "CountZeros", "Default");
+    REGISTER_NARY(EqualConstraint, "Equal", "Default");
+    REGISTER_NARY(EqualProductConstraint, "EqualProduct", "Default");
+    REGISTER_NARY(EqualSumConstraint, "EqualSum", "Default");
+    REGISTER_NARY(LessOrEqThanSumConstraint, "LessOrEqThanSum", "Default");
+    REGISTER_NARY(LessThanConstraint, "LessThan", "Default");
+    REGISTER_NARY(LessThanEqualConstraint, "LessThanEqual", "Default");
+    REGISTER_NARY(LessThanSumConstraint, "LessThanSum", "Default");
     REGISTER_NARY(MemberImplyConstraint, "MemberImply", "Default");
+    REGISTER_NARY(MultEqualConstraint, "MultEqual", "Default");
+    REGISTER_NARY(NotEqualConstraint, "NotEqual", "Default");
 
     // Europa (NewPlan/ConstraintNetwork) names for the same constraints:
     REGISTER_NARY(AddEqualConstraint, "addeq", "Default");
     REGISTER_NARY(AddMultEqualConstraint, "addmuleq", "Default");
     REGISTER_NARY(EqualConstraint, "asame", "Default"); // all same
+    REGISTER_NARY(CardinalityConstraint, "card", "Default"); // cardinality not more than
+    REGISTER_NARY(CountNonZerosConstraint, "cardeq", "Default"); // cardinality equals
     REGISTER_NARY(CondAllSameConstraint, "condeq", "Default");
     REGISTER_NARY(EqualConstraint, "eq", "Default");
+    REGISTER_NARY(EqualConstraint, "fasame", "Default"); // flexible all same
     REGISTER_NARY(LessThanEqualConstraint, "leq", "Default");
     REGISTER_NARY(LessOrEqThanSumConstraint, "leqsum", "Default");
     REGISTER_NARY(LessThanConstraint, "lt", "Default");
