715380891f9634d9bfc0f2a14034c74668410ff8
Conor Mcgann <mcgann.conor@gmail.com>
2004-03-24 17:58:30 +0000

    Added code to enforce assumptions about maximum number of strings and minimum memory addresses allocated in Id's. Also moved around code for declaring error checks
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@1536 23e1bc29-d954-0410-9766-47de34158ba0

--- Utils/CommonDefs.hh
+++ Utils/CommonDefs.hh
@@ -1,9 +1,57 @@
 #ifndef _H_CommonDefs
 #define _H_CommonDefs
 
-namespace Prototype {
+#include <cassert>
 
-  //#define PROTOTYPE_FAST_VERSION
+
+//#define PROTOTYPE_FAST_VERSION
+
+/**
+ * @brief Condition indicating the surrounding call to check_error() or similar should always fail.
+ * @note Should only be used as an argument to check_error(), assert(), assertTrue(), etc.
+ * @note Note also that assert() should only be used in test programs and not even there in the long term,
+ *   since some compilers implement it using a #define, which prevents setting a break point in it.
+ * --wedgingt 2004 Mar 3
+ * @note Why is this a C++ variable rather than a #define? --wedgingt 2004 Mar 3
+ */
+const bool ALWAYS_FAILS = false;
+
+/**
+ * @def check_error
+ * @brief If the condition is false, generate an error.
+ * @param cond The condition to test.
+ * @note Current, preliminary, implementation simply calls assert().
+ * @note Once the new error handling support is is use, this will instead
+ *   throw an exception when the condition is false.
+ * @note Should only be used in 'core' code and in test programs when the
+ *   condition cannot be tested when the 'core' code has been compiled 'fast'.
+ *   Otherwise - in test programs where the condition can always be checked,
+ *   no matter how the code was compiled - use assertTrue(), assertFalse(), etc.
+ * @see assertTrue, assertFalse, ALWAYS_FAIL
+ */
+#ifndef PROTOTYPE_FAST_VERSION
+#define check_error(cond) assert(cond)
+#else
+#define check_error(cond)
+#endif
+
+/**
+ * @def assertTrue
+ * @brief Require the condition to be true, aborting the program otherwise.
+ * @note Should only be used in test programs.
+ * @note Likely precursor to new error handling support.
+ */
+#define assertTrue(cond) assert(cond)
+
+/**
+ * @def assertFalse
+ * @brief Require the condition to be false, aborting the program otherwise.
+ * @note Should only be used in test programs.
+ * @note Likely precursor to new error handling support.
+ */
+#define assertFalse(cond) assert(!(cond))
+
+namespace Prototype {
 
   /**
    * @def DECLARE_GLOBAL_CONST(TYPE,NAME)
@@ -46,6 +94,10 @@ namespace Prototype {
   #define MAX_INT (g_maxInt())
   #define PLUS_INFINITY (g_infiniteTime())
   #define MINUS_INFINITY (-g_infiniteTime())
-  #define EPSILON (g_epsilon()) /**< Used when computing differences and comparing real numbers: smallest recognized increment. */
+  #define EPSILON (g_epsilon()) /*!< Used when computing differences and comparing real numbers: smallest recognized increment. */
+
+  /*!< Magic number to enforce assumption that memory addresses and string keys will never collide */
+  #define ASSUMED_MINIMUM_MEMORY_ADDRESS 1000000 
+  #define MAXIMUM_STRING_COUNT ASSUMED_MINIMUM_MEMORY_ADDRESS 
 }
 #endif
