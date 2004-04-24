de776e8dc5f3cb1aab393e0fef148827ab6365e9
Will Edgington <no_author@no_author.com>
2004-04-24 00:34:32 +0000

    Improve indentation, notably by changing tabs to spaces.  Fix error condition in processScope() which was both causing an error inappropriately and not detecting a different kind of error.  Add comment about additional propagation that could be done.  Add another comment about what I believe to be a bug but am less sure about; I will try to add a test to check.  Hm, looks like I missed a typo at the end of the comment near line 80.
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@1614 23e1bc29-d954-0410-9766-47de34158ba0

--- ConstraintEngine/component/EqualityConstraintPropagator.cc
+++ ConstraintEngine/component/EqualityConstraintPropagator.cc
@@ -66,9 +66,9 @@ namespace Prototype {
   }
 
   void EqualityConstraintPropagator::handleNotification(const ConstrainedVariableId& variable, 
-							int argIndex, 
-							const ConstraintId& constraint, 
-							const DomainListener::ChangeType& changeType){
+                                                        int argIndex, 
+                                                        const ConstraintId& constraint, 
+                                                        const DomainListener::ChangeType& changeType){
     check_error(Id<EqualConstraint>::convertable(constraint));
 
     if(!m_fullReprop && !m_active){
@@ -79,40 +79,59 @@ namespace Prototype {
     }
   }
 
-  void processScope(const std::set<ConstrainedVariableId>& scope){
+  void processScope(const std::set<ConstrainedVariableId>& scope) {
     AbstractDomain& domain(EqualConstraint::getCurrentDomain(* (scope.begin())));
 
-    if(domain.isOpen())
+    if (domain.isOpen())
       return;
 
-    // Set up the initial values to match others against
+    // Set up the initial values to match others against.
     bool isFinite = domain.isFinite();
-    int domainType = domain.getType();
 
-    // Iterate over, restricting domain as we go
-    for(std::set<ConstrainedVariableId>::const_iterator it = scope.begin(); it != scope.end(); ++it){
+    // int domainType = domain.getType(); // Unused; see below.
+
+    // Iterate over, restricting domain as we go.
+    for (std::set<ConstrainedVariableId>::const_iterator it = scope.begin(); it != scope.end(); ++it) {
       AbstractDomain& currentDomain = EqualConstraint::getCurrentDomain(*it);
-      check_error(currentDomain.getType() == domainType);
+      
+      // This next check is incorrect: could be false when they can be
+      // compared (real enumerations and integer intervals, e.g.) and
+      // could be true when they cannot be compared (e.g., two distinct
+      // label sets, which both have a DomainType of USER_DEFINED).
+      // The correct check follows.
+      // --wedgingt@ptolemy.arc.nasa.gov 2004 Apr 21
+      // check_error(currentDomain.getType() == domainType);
 
-      if(currentDomain.isOpen())
-	return;
+      check_error(AbstractDomain::canBeCompared(domain, currentDomain));
 
-      if((currentDomain.isFinite() != isFinite) || 
-	 domain.intersect(currentDomain) && domain.isEmpty()){
-	currentDomain.empty();
-	return;
+      // This will preclude possible propagation to the point of allowing
+      // an arbitrary amount of useless search during planning, including
+      // not even intersecting with closed domains later in the scope.
+      // --wedgingt@ptolemy.arc.nasa.gov 2004 Apr 21
+      if (currentDomain.isOpen())
+        return;
+
+      // What if the domains are (numeric) [-Inf Inf] and (integer) [0 1] ?
+      // This would appear to incorrectly provoke an inconsistency in that case.
+      // --wedgingt@ptolemy.arc.nasa.gov 2004 Apr 22
+      if (currentDomain.isFinite() != isFinite || 
+          domain.intersect(currentDomain) && domain.isEmpty()) {
+        currentDomain.empty();
+        return;
       }
     }
 
-    // If we get to here, we have computed the new domain for all variables in the scope and we know that no
-    // domain has been emptied (this could be optimized by recording the last change to dommain)
-    for(std::set<ConstrainedVariableId>::const_iterator it = scope.begin(); it != scope.end(); ++it){
+    // If we get to here, we have computed the new domain for all
+    // variables in the scope and we know that no domain has been
+    // emptied (this could be optimized by recording the last change
+    // to dommain).
+    for (std::set<ConstrainedVariableId>::const_iterator it = scope.begin(); it != scope.end(); ++it) {
       AbstractDomain& currentDomain = EqualConstraint::getCurrentDomain(*it);
       currentDomain.intersect(domain);
     }
   }
 
-  void EqualityConstraintPropagator::equate(const std::set<ConstrainedVariableId>& scope){
+  void EqualityConstraintPropagator::equate(const std::set<ConstrainedVariableId>& scope) {
     check_error(!scope.empty());
     processScope(scope);
   }
