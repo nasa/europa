9d2b9a3664864e37d7250e0d3770b4087daeae08
Tania Bedrax-Weiss <no_author@no_author.com>
2004-07-02 16:15:26 +0000

    removed object decision point comparator since it is not being used
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@1765 23e1bc29-d954-0410-9766-47de34158ba0

--- CBPlanner/component/ObjectDecisionPoint.hh
+++ CBPlanner/component/ObjectDecisionPoint.hh
@@ -3,7 +3,6 @@
 
 #include "CBPlannerDefs.hh"
 #include "DecisionPoint.hh"
-#include "Token.hh" // for the comparator below
 
 namespace Prototype {
 
@@ -27,19 +26,6 @@ namespace Prototype {
     ObjectId m_object;
     TokenId m_token;
   };
-
-  /**
-   * @brief Token Decision Points need to be compared with object key and
-   * token key.  Comparator for ObjectDecisionSet.
-   */
-  class ObjectDecisionPointComparator {
-  public:
-    bool operator() (const ObjectDecisionPointId& o1, const ObjectDecisionPointId& o2) const {
-      return o1->getEntityKey() == o2->getEntityKey() && o1->getToken()->getKey() == o2->getToken()->getKey();
-    }
-    bool operator==(const ObjectDecisionPointComparator& c) {return true;}
-  };
-
 
   std::ostream& operator <<(std::ostream& os, const Id<ObjectDecisionPoint>&);
 
