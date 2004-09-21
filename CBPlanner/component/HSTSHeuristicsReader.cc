a9970c018a24ec0d7a97e4f3d2af5611092ee5c6
Tania Bedrax-Weiss <no_author@no_author.com>
2004-09-21 00:17:35 +0000

    began implementation of the Heuristics Reader
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@1960 23e1bc29-d954-0410-9766-47de34158ba0

---
+++ CBPlanner/component/HSTSHeuristicsReader.cc
@@ -0,0 +1,86 @@
+#include "HSTSHeuristicsReader.hh"
+
+namespace Prototype {
+
+  HSTSHeuristicsReader::HSTSHeuristicsReader(HSTSHeuristics& heuristics) : m_heuristics(heuristics) { }
+
+  ~HSTSHeuristicsReader::HSTSHeuristicsReader() { }
+
+  void HSTSHeuristicsReader::read(const std::string& fileName) {
+
+    std::ifstream is(fileName.c_str());
+
+    int elements = 0;
+    while (!is.eof()) {
+      if (is.peek() != '<') {
+	is.get(); // discard characters up to '<'
+	continue;
+      }
+      TiXmlElement element("");
+      is >> element;
+      const char * tagname = element.Value();    
+      if (strcmp(tagname, "Document") == 0) {
+	for (TiXmlElement* child = element.FirstChildElement(); child;
+	     child = child->NextSiblingElement()) {
+	  check_error(child != NULL);
+	  readElement(*child);
+	}
+      }
+      else
+	readElement(element);
+      elements++;
+    }
+  }
+
+  void HSTSHeuristicsReader::readElement(const TiXmlElement& element) {
+    const char * tagName = element.Value();
+    if (strcmp(tagName, "Defaults") == 0)
+      readDefaults(element);
+    else if (strcmp(tagName, "VariableSpecification") == 0)
+      readVariableSpecification(element);
+    else if (strcmp(tagName, "TokenSpecification") == 0)
+      readTokenSpecification(element);
+  }
+
+  void HSTSHeuristicsReader::readDefaults(const TiXmlElement& element) {
+    for (TiXmlElement* child = element.FirstChildElement(); child;
+	 child = child->NextSiblingElement()) {
+      check_error(child != NULL);
+      const char* tagName = child->Value();
+      check_error(tagName != NULL);
+      if (strcmp(tagName, "PriorityPref") == 0) 
+	readPriorityPref(*child);
+      else if (strcmp(tagName, "Compatibility") == 0)
+	readCompatibility(*child);
+      else if (strcmp(tagName, "Token") == 0)
+	readToken(*child);
+      else if (strcmp(tagName, "ConstrainedVariable") == 0)
+	readConstrainedVariable(*child);
+    }
+  }
+
+  const std::string HSTSHeuristicsReader::getTextChild (const TiXmlElement& element) {
+    check_error (element && (element->FirstChild()) && (element->FirstChild()->ToText()) && (element->FirstChild()->ToText()->Value()));
+    return element->FirstChild()->ToText()->Value();
+  }
+
+  void HSTSHeuristicsReader::readPriorityPref(const TiXmlElement& element) {
+    std::string tmp = getNextChild(element);
+    std::cout << "readPriorityPref = " << tmp << std::endl;
+    m_heuristics.setDefaultPriorityPreference(getNextChild(element));
+    std::cout << "getting PriorityPref = " << m_heuristics.getDefaultPriorityPreference() << std::endl;
+  }
+
+  void HSTSHeuristicsReader::readVariableSpecification(const TiXmlElement& element){}
+  void HSTSHeuristicsReader::readTokenSpecification(const TiXmlElement& element){}
+  void HSTSHeuristicsReader::readCompatibility(const TiXmlElement& element){}
+  void HSTSHeuristicsReader::readPredicateSpec(const TiXmlElement& element){}
+  void HSTSHeuristicsReader::readDecisionPreference(const TiXmlElement& element){}
+  void HSTSHeuristicsReader::readConstrainedVariable(const TiXmlElement& element){}
+  void HSTSHeuristicsReader::readPreference(const TiXmlElement& element){}
+  void HSTSHeuristicsReader::readMaster(const TiXmlElement& element){}
+
+  GeneratorId HSTSHeuristicsReader::getGeneratorFromName(std::string genName){}
+}
+
+#endif
---
+++ CBPlanner/component/HSTSHeuristicsReader.hh
@@ -0,0 +1,43 @@
+#ifndef _H_HeuristicsHSTSReader
+#define _H_HeuristicsHSTSReader
+
+#include "CBPlannerDefs.hh"
+#include <iostream>
+
+class TiXmlElement;
+
+namespace Prototype {
+
+  class HeuristicsHSTSReader {
+  public:
+    HeuristicsHSTSReader(HSTSHeuristics& heuristics);
+    ~HeuristicsHSTSReader();
+
+    void read(const std::string& fileName);
+  protected:
+    void readElement(const TiXmlElement& element);
+    void readDefaults(const TiXmlElement& element);
+    void readPriorityPref(const TiXmlElement& element);
+    void readVariableSpecification(const TiXmlElement& element);
+    void readTokenSpecification(const TiXmlElement& element);
+    void readCompatibility(const TiXmlElement& element);
+    void readPredicateSpec(const TiXmlElement& element);
+    void readDecisionPreference(const TiXmlElement& element);
+    void readConstrainedVariable(const TiXmlElement& element);
+    void readPreference(const TiXmlElement& element);
+    void readMaster(const TiXmlElement& element);
+  private:
+    HSTSHeuristics m_heuristics;
+
+    const std::string getTextChild(const TiXmlElement& element);
+    GeneratorId getGeneratorFromName(std::string genName);
+
+    /*    
+	  ConstrainedVariableId parseVariable(std::string variable);
+	  ConstrainedVariableId parseToken(std::string token);
+    */
+   
+  };
+}
+
+#endif
