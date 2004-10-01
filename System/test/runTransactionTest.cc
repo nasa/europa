738e0dd6b5602b1c8bafe9a32639248623199b81
Tania Bedrax-Weiss <no_author@no_author.com>
2004-10-01 00:45:38 +0000

    adapted for Resource code
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@2016 23e1bc29-d954-0410-9766-47de34158ba0

--- System/test/runTransactionTest.cc
+++ System/test/runTransactionTest.cc
@@ -1,3 +1,4 @@
+
 // For performance tests only
 #include "PrototypePerformanceConstraint.hh"
 
@@ -5,12 +6,19 @@
 #include "Nddl.hh"
 #include "SamplePlanDatabase.hh"
 
+// For basic types
+#include "TypeFactory.hh"
+
 // Support for planner
 #include "CBPlanner.hh"
 #include "DecisionPoint.hh"
-
+#include "ResourceOpenDecisionManager.hh"
 #include "PlanDatabaseWriter.hh"
+
 #include "Constraints.hh"
+
+// For resources tests only
+#include "LoraxConstraints.hh"
 
 #include <fstream>
 
@@ -31,65 +39,70 @@ bool replay = true;
 const char * initialTransactions = NULL;
 
 bool runPlanner(){
-    SamplePlanDatabase db1(schema, replay);
+  SamplePlanDatabase db1(schema, replay);
 
-    DbClientId client = db1.planDatabase->getClient();
+  // Set ResourceOpenDecisionManager
+  DecisionManagerId local_dm = db1.planner->getDecisionManager();
+  ResourceOpenDecisionManagerId local_rodm = (new ResourceOpenDecisionManager(local_dm))->getId();
+  local_dm->setOpenDecisionManager( local_rodm );
 
-    DbClientTransactionPlayer player(client);
-    check_error(initialTransactions != NULL);
-    std::ifstream in(initialTransactions);
+  DbClientId client = db1.planDatabase->getClient();
+
+  DbClientTransactionPlayer player(client);
+  check_error(initialTransactions != NULL);
+  std::ifstream in(initialTransactions);
+  player.play(in);
+
+  assert(client->propagate());
+
+  ObjectId world = client->getObject(LabelStr("world"));
+  check_error(world.isValid());
+  // Set up the horizon  from the model now. Will cause a refresh of the query, but that is OK.
+  ConstrainedVariableId horizonStart = world->getVariable(LabelStr("world.m_horizonStart"));
+  check_error(horizonStart.isValid());
+  ConstrainedVariableId horizonEnd = world->getVariable(LabelStr("world.m_horizonEnd"));
+  check_error(horizonEnd.isValid());
+  int start = (int) horizonStart->baseDomain().getSingletonValue();
+  int end = (int) horizonEnd->baseDomain().getSingletonValue();
+  db1.horizon->setHorizon(start, end);
+
+  // Create and run the planner
+  ConstrainedVariableId maxPlannerSteps = world->getVariable(LabelStr("world.m_maxPlannerSteps"));
+  check_error(maxPlannerSteps.isValid());
+  int steps = (int) maxPlannerSteps->baseDomain().getSingletonValue();
+
+  testLangInit(db1.planDatabase, db1.planner->getDecisionManager(),
+	       db1.constraintEngine, db1.rulesEngine);
+
+  int res = db1.planner->run(steps);
+
+  testLangDeinit();
+
+  assert(res == CBPlanner::PLAN_FOUND);
+
+  PlanDatabaseWriter::write(db1.planDatabase, std::cout);
+
+  // Store transactions for recreation of database
+  if(replay) {
+    std::stringstream os1;
+    db1.planDatabase->getClient()->toStream(os1);
+    std::ofstream out(TX_LOG);
+    db1.txLog->flush(out);
+    out.close();
+
+    std::stringstream os2;
+    SamplePlanDatabase db(schema, true);
+    DbClientTransactionPlayer player(db.planDatabase->getClient());
+    std::ifstream in(TX_LOG);
     player.play(in);
+    db.planDatabase->getClient()->toStream(os2);
 
-    assert(client->propagate());
+    std::string s1 = os1.str();
+    std::string s2 = os2.str();
+    assert(s1 == s2);
+  }
 
-    ObjectId world = client->getObject(LabelStr("world"));
-    check_error(world.isValid());
-    // Set up the horizon  from the model now. Will cause a refresh of the query, but that is OK.
-    ConstrainedVariableId horizonStart = world->getVariable(LabelStr("world.m_horizonStart"));
-    check_error(horizonStart.isValid());
-    ConstrainedVariableId horizonEnd = world->getVariable(LabelStr("world.m_horizonEnd"));
-    check_error(horizonEnd.isValid());
-    int start = (int) horizonStart->baseDomain().getSingletonValue();
-    int end = (int) horizonEnd->baseDomain().getSingletonValue();
-    db1.horizon->setHorizon(start, end);
-
-    // Create and run the planner
-    ConstrainedVariableId maxPlannerSteps = world->getVariable(LabelStr("world.m_maxPlannerSteps"));
-    check_error(maxPlannerSteps.isValid());
-    int steps = (int) maxPlannerSteps->baseDomain().getSingletonValue();
-
-    testLangInit(db1.planDatabase, db1.planner->getDecisionManager(),
-                 db1.constraintEngine, db1.rulesEngine);
-
-    int res = db1.planner->run(steps);
-
-    testLangDeinit();
-
-    assert(res == CBPlanner::PLAN_FOUND);
-
-    PlanDatabaseWriter::write(db1.planDatabase, std::cout);
-
-    // Store transactions for recreation of database
-    if(replay) {
-      std::stringstream os1;
-      db1.planDatabase->getClient()->toStream(os1);
-      std::ofstream out(TX_LOG);
-      db1.txLog->flush(out);
-      out.close();
-
-      std::stringstream os2;
-      SamplePlanDatabase db(schema, true);
-      DbClientTransactionPlayer player(db.planDatabase->getClient());
-      std::ifstream in(TX_LOG);
-      player.play(in);
-      db.planDatabase->getClient()->toStream(os2);
-
-      std::string s1 = os1.str();
-      std::string s2 = os2.str();
-      assert(s1 == s2);
-    }
-
-    return true;
+  return true;
 }
 
 bool copyFromFile(){
