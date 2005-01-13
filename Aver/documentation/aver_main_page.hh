95cb17e72793f6666308ea3c94db257c8ef502ae
Tania Bedrax-Weiss <no_author@no_author.com>
2005-01-13 21:36:18 +0000

    new
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@2487 23e1bc29-d954-0410-9766-47de34158ba0

---
+++ Aver/documentation/aver_main_page.hh
@@ -0,0 +1,9 @@
+/**
+ * @defgroup Aver
+ * <A NAME="aver_overview"></A>
+ * Briefly describe the functionality
+ * <A NAME="aver_entry"><H3>Aver API</H3></A>
+ * Indicate the main entry points to the API
+ * <A NAME="aver_use"><H3>Using the Aver API</H3></A>
+ * Indicate tests that provide examples of usage
+ */
