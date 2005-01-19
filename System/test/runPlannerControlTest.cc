ca0db1ea23270c1b82a10b5546ce18be9ec284f2
Patrick Daley <no_author@no_author.com>
2005-01-19 22:12:21 +0000

    removed references to lock
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@2526 23e1bc29-d954-0410-9766-47de34158ba0

--- System/test/runPlannerControlTest.cc
+++ System/test/runPlannerControlTest.cc
@@ -3,11 +3,7 @@
 #include <stdio.h>
 #include <dlfcn.h>
 
-#include "LockManager.hh"
-
 int main(int argc, const char ** argv) {
-  LockManager::connect();
-  LockManager::lock();
 
   const char* plannerLibPath;
   const char* initialStatePath;
