f5dd281d651b62e8398ec535e7d7a1e91cf7bce6
Tania Bedrax-Weiss <no_author@no_author.com>
2005-02-25 00:59:42 +0000

    added fully scoped variable names for matching
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@2684 23e1bc29-d954-0410-9766-47de34158ba0

--- HSTS/component/HSTSNoBranchCondition.cc
+++ HSTS/component/HSTSNoBranchCondition.cc
@@ -11,16 +11,34 @@ namespace EUROPA {
 
   void HSTSNoBranchCondition::initialize(const HSTSNoBranchId& noBranchSpec) {
     m_noBranches = noBranchSpec->getNoBranchSpec();
-    notifyChanged();
+    if (!m_noBranches.empty()) {
+      std::cout << " No Branch is NOT empty" << std::endl;
+      notifyChanged();
+    }
+    else { std::cout << " No Branch is empty" << std::endl; }
+    
   }
 
   bool HSTSNoBranchCondition::test(const EntityId& entity) {
     check_error(entity.isValid());
     if (!ConstrainedVariableId::convertable(entity)) return true;
     ConstrainedVariableId var(entity);
-    std::set<LabelStr>::iterator it = m_noBranches.find(var->getName());
-    if (it != m_noBranches.end())
+
+    std::cout << " NoBranches has " << (*m_noBranches.begin()).c_str() << std::endl;
+
+    std::string varName = var->getName().c_str();
+    if (!var->getParent().isNoId()) {
+      std::string parentName = var->getParent()->getName().c_str();
+      varName = parentName + "." + varName;
+    }
+
+    std::cout << " Testing variable (" << var->getKey() << ") " << varName;
+    std::set<LabelStr>::iterator it = m_noBranches.find(varName);
+    if (it != m_noBranches.end()) {
+      std::cout << " matched NO_BRANCH" << std::endl;
       return false;
+    }
+    std::cout << " didn't match NO_BRANCH" << std::endl;
     return true;
   }
 
