f42c8575ba2b521497ed64af91d946eb38352606
Tania Bedrax-Weiss <no_author@no_author.com>
2005-03-29 00:54:28 +0000

    removed some bogus debug messages
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@2724 23e1bc29-d954-0410-9766-47de34158ba0

--- HSTS/component/HSTSNoBranchCondition.cc
+++ HSTS/component/HSTSNoBranchCondition.cc
@@ -13,20 +13,14 @@ namespace EUROPA {
   void HSTSNoBranchCondition::initialize(const HSTSNoBranchId& noBranchSpec) {
     m_noBranches = noBranchSpec->getNoBranchSpec();
     if (!m_noBranches.empty()) {
-      debugMsg("HSTSNoBranchCondition"," No Branch is NOT empty");
       notifyChanged();
     }
-    else { debugMsg("HSTSNoBranchCondition"," No Branch is empty"); }
-    
   }
 
   bool HSTSNoBranchCondition::test(const EntityId& entity) {
     check_error(entity.isValid());
     if (!ConstrainedVariableId::convertable(entity)) return true;
     ConstrainedVariableId var(entity);
-
-    debugMsg("HSTSNoBranchCondition"," NoBranches has " << (*m_noBranches.begin()).c_str());
-
     std::string varName = var->getName().c_str();
     if (!var->getParent().isNoId()) {
       std::string parentName = var->getParent()->getName().c_str();
