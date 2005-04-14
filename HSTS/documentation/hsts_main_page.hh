e43e7d9a3a8ccb838a91ba883311c9bf5ffdbe01
No Author <no_author@no_author.com>
2005-04-14 21:40:04 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.10.2'.
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/branches/unlabeled-1.10.2@2755 23e1bc29-d954-0410-9766-47de34158ba0

--- HSTS/documentation/hsts_main_page.hh
+++
@@ -1,9 +0,0 @@
-/**
- * @defgroup HSTS
- * <A NAME="hsts_overview"></A>
- * Briefly describe the functionality
- * <A NAME="hsts_entry"><H3>HSTS Compatibility API</H3></A>
- * Indicate the main entry points to the API
- * <A NAME="hsts_use"><H3>Using the HSTS Compatibility API</H3></A>
- * Indicate tests that provide examples of usage
- */
