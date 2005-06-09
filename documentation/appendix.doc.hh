c47ab8249aba3d091f83a8a440c82001a7e398da
Conor Mcgann <mcgann.conor@gmail.com>
2005-06-09 23:01:21 +0000

    First drop of HelloWorld
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@2841 23e1bc29-d954-0410-9766-47de34158ba0

---
+++ documentation/appendix.doc.hh
@@ -0,0 +1,7 @@
+/**
+ * @page appendix Appendix
+ * @section readme README
+ * @include ../README
+ * @section building BUILDING @ref europa "EUROPA"
+ * @include ../BUILDING
+ */
--- documentation/solvers.doc.hh
+++ documentation/solvers.doc.hh
@@ -1,4 +1,5 @@
 /**
  * @page solvers Building and Using Problem Solvers
  * This is where we talk about solvers.
+ * @section solverConfig Solver Configuration
  */
