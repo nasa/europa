aae35ec7dea9d6d392e597d25ab89d8180c68b81
No Author <no_author@no_author.com>
2005-07-06 19:03:31 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.34.2'.
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/branches/unlabeled-1.34.2@2901 23e1bc29-d954-0410-9766-47de34158ba0

--- System/documentation/sys_main_page.hh
+++
@@ -1,9 +0,0 @@
-/**
- * @defgroup System
- * @section sys_overview Overview
- * Briefly describe the functionality
- * @section sys_use Using the System module
- * Briefly state how to use the system module
- * @section sys_customize Customizing the System module
- * Briefly describe how to customize the system module if appropriate
- */
