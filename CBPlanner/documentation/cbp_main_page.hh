6154764d858b79f0ece7037e116b1489b5d435c6
No Author <no_author@no_author.com>
2005-08-04 22:51:27 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.17.2'.
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/branches/unlabeled-1.17.2@2918 23e1bc29-d954-0410-9766-47de34158ba0

--- CBPlanner/documentation/cbp_main_page.hh
+++
@@ -1,9 +0,0 @@
-/**
- * @defgroup CBPlanner
- * <A NAME="cbp_overview"></A>
- * Briefly describe the functionality
- * <A NAME="cbp_entry"><H3>CBPlanner API</H3></A>
- * Indicate the main entry points to the API
- * <A NAME="cbp_use"><H3>Using the CBPlanner API</H3></A>
- * Indicate tests that provide examples of usage
- */
