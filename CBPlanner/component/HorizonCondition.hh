f56078472d1908495195c8d8cf71ae69f5f8584b
Peter Jarvis <no_author@no_author.com>
2005-08-11 21:30:40 +0000

    2998: Added documentation to class
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@2935 23e1bc29-d954-0410-9766-47de34158ba0

--- CBPlanner/component/HorizonCondition.hh
+++ CBPlanner/component/HorizonCondition.hh
@@ -12,11 +12,36 @@ namespace EUROPA {
   public:
     HorizonCondition(const HorizonId& hor, const DecisionManagerId& dm);
     virtual ~HorizonCondition();
-
+   
+    /**
+     * @brief Specifies that we want to filter tokens that are necessarily outside
+     * the planning horizon
+     */
     void setNecessarilyOutsideHorizon();
+    
+    /**
+     * @brief Specifies that we want to filter tokens that are necessarily outside
+     * the planning horizon
+     */
     void setPossiblyOutsideHorizon();
+    
+    /**
+     * @brief returns true iff we are set to use the possibly filter defintion.
+     */
     bool isPossiblyOutsideHorizon() const;
+
+   /**
+     * @brief returns true iff we are set to use the necessary filter defintion.
+     */
     bool isNecessarilyOutsideHorizon() const;
+
+    /**
+     * @breif return true if token occurs "within" timeline, false otherwise. 
+     * The defintion of "within" used is controlled by the setNecessarilyOutsideHorizon or
+     * setPossiblityOutsideHorizon methods.
+     * @param tok The id of the token we wish to test. Method will attempt to convert entity
+     * to a tokenid. 
+     */   
 
     bool test(const EntityId& entity);
 
@@ -25,7 +50,7 @@ namespace EUROPA {
     bool testToken(const TokenId& tok);
 
     HorizonId m_horizon;
-    bool m_possiblyOutside;
+    bool m_possiblyOutside; /* flag controls which defintion of "within" horizon we use */
   };
 
 } /* namespace Europa */
