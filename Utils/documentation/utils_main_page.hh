4af1de2a5af7a3149164bb2d600aca20663cda0f
No Author <no_author@no_author.com>
2005-09-08 08:33:39 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.141.2'.
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/branches/unlabeled-1.141.2@3031 23e1bc29-d954-0410-9766-47de34158ba0

--- Utils/documentation/utils_main_page.hh
+++
@@ -1,9 +0,0 @@
-/**
- * @defgroup Utils
- * <A NAME="utils_overview"></A>
- * Briefly describe the functionality
- * <A NAME="utils_entry"><H3>Utilities API</H3></A>
- * Indicate the main entry points to the API
- * <A NAME="utils_use"><H3>Using the Utilities API</H3></A>
- * Indicate tests that provide examples of usage
- */
