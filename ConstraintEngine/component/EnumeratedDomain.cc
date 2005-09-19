e915c108ae013f0fb4a0bcebd59c403afbdc4b03
Michael Iatauro <michael.j.iatauro@nasa.gov>
2005-09-19 22:19:35 +0000

    rollback of cbplanner-htx work
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/branches/unlabeled-1.54.2@3105 23e1bc29-d954-0410-9766-47de34158ba0

--- ConstraintEngine/component/EnumeratedDomain.cc
+++ ConstraintEngine/component/EnumeratedDomain.cc
@@ -491,17 +491,13 @@ namespace EUROPA {
       if (isNumeric()) {
         os << comma << valueAsDouble;
         comma = ", ";
-      } 
-      else if (valueAsDouble == true)
-	orderedSet.insert("true");
-      else if (valueAsDouble == false)
-	orderedSet.insert("false");
-      else if (LabelStr::isString(valueAsDouble))
-	orderedSet.insert(LabelStr(valueAsDouble).toString());
-      else {
-	EntityId entity(valueAsDouble);
-	orderedSet.insert(entity->getName().toString());
-      }
+      } else
+        if (LabelStr::isString(valueAsDouble))
+          orderedSet.insert(LabelStr(valueAsDouble).toString());
+        else {
+          EntityId entity(valueAsDouble);
+          orderedSet.insert(entity->getName().toString());
+        }
     }
 
     for (std::set<std::string>::const_iterator it = orderedSet.begin(); it != orderedSet.end(); ++it) {
