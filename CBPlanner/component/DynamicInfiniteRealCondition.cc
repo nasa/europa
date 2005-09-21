9326d67aecdd5caddd850c21757580c7ac1b23aa
Peter Jarvis <no_author@no_author.com>
2005-09-21 23:06:20 +0000

    3084: added ability to configure if dynamic objects are filtered together with methods for getting and setting configuration
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@3123 23e1bc29-d954-0410-9766-47de34158ba0

--- CBPlanner/component/DynamicInfiniteRealCondition.cc
+++ CBPlanner/component/DynamicInfiniteRealCondition.cc
@@ -4,8 +4,9 @@
 
 namespace EUROPA {
 
-  DynamicInfiniteRealCondition::DynamicInfiniteRealCondition(const DecisionManagerId& dm): Condition(dm) {
+  DynamicInfiniteRealCondition::DynamicInfiniteRealCondition(const DecisionManagerId& dm): Condition(dm), m_applyDynamicExclusion(true) {
     check_error(m_id.isValid());
+
   }
 
   DynamicInfiniteRealCondition::~DynamicInfiniteRealCondition() { }
@@ -16,11 +17,26 @@ namespace EUROPA {
     check_error(var.isValid());
     bool passed(true);
     const AbstractDomain& dom = var->lastDomain();
-    if (dom.isOpen() || dom.isInfinite())
-      passed = false;
+  
+    if (m_applyDynamicExclusion) {
+        if (dom.isOpen() || dom.isInfinite())
+           passed = false;
+    } else {
+        if (dom.isInfinite())
+           passed = false;
+    }
     return passed;
   } 
 	      
   inline void DynamicInfiniteRealCondition::print (std::ostream& os) { os << "DYNAMIC_INFINITE_REAL_CONDITION"; }
+
+  void DynamicInfiniteRealCondition::disableDynamicExclusion() {
+    check_error(m_applyDynamicExclusion, "Default value for m_applyDynamicExclusion must be true. Value is false");
+    m_applyDynamicExclusion = false;
+  }
+
+  bool DynamicInfiniteRealCondition::isDynamicExclusionEnabled() {
+    return m_applyDynamicExclusion;
+  }
 
 } 
