a144bb043760efd4de91d6d8f2baa4a3adff0fd0
Peter Jarvis <no_author@no_author.com>
2005-10-11 22:35:59 +0000

    inital version of file with toc listed
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@3198 23e1bc29-d954-0410-9766-47de34158ba0

---
+++ documentation/users_guide/usersguide.doc.hh
@@ -0,0 +1,37 @@
+/**
+ * @page usersguide Users' Guide
+ *
+ * <ul>
+ * <li>Introduction
+ *     <ul>
+ *     <li>Automated planning
+ *     <li>EUROPA2 goals  
+ *     <li>Users' Guide overview 
+ *     </ul>
+ * <li>Tutorial
+ *     <ul>
+ *     <li>Hello world example
+ *     <li>Plantary Rover Example
+ *       <ul>
+ *          <li>Domain modelling in NDDL
+ *          <li>Planning with PLASMA
+ *          <li>Inspecting with PlanWorks
+ *       </ul>
+ *     </ul>
+ * <li>Reference
+ *     <ul>
+ *       <li> System architecture
+ *       <li> CSP Primer
+ *       <li> NDDL Reference Manaual
+ *       <li> Solver Reference Manaual
+ *       <li> Plan Database Reference Manaual
+ *       <li> System Reference Manual
+ *       <ul>
+ *         <li>Building, running, tracing.
+ *         <li> debugging
+ *         <li> advanced building
+ *         <li> profiling.
+ *       </ul>
+ *     </ul>
+ * </ul>
+ */
