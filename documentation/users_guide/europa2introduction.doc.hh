9613485de9654e604b26c2c47934da373fd968e4
Peter Jarvis <no_author@no_author.com>
2005-10-12 21:42:15 +0000

    removing files moved to depricated or pictures or nddl_examples directories
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@3220 23e1bc29-d954-0410-9766-47de34158ba0

---
+++ documentation/users_guide/europa2introduction.doc.hh
@@ -0,0 +1,20 @@
+/**
+ * @page europa2introduction EUROPA2
+ *
+ * @section whatiseuropa Objective
+ *
+ * EUROPA2 is a fast, flexible, extensible, and resusable technology platform for building planning and scheduling applications for space exploration. Its predecessor, EUROPA, 
+ * has been deployed as the core planning technology for a varity of NASA research and mission applications. A notable example is MAPGEN, the ground-based
+ * activity planning system for the Mars Exploration Rover (MER) mission. EUROPA2 is reimplementation of EUROPA that is significantly faster and easily exstended through a focus on state-of-the art software enginnering.  
+ * 
+ * EUROPA2 (Extensible Universal Remote Operations Architecture) is a component-based sofware library for representing and reasoning with plans. It is analogious to 
+ * component-based sofware in over areas like CLARATy for robotoics or ILOG for scheduling.  
+ *
+ *@section usecases Use-Cases
+ * There are three major use-cases for deploying EUROPA2. 
+ * <ul>
+ * <li><b>Batch Planner</b>. 
+ * </ul>
+ * @image html Applications.jpg Uses Cases
+ * 
+ */
--- documentation/users_guide/usersguide.doc.hh
+++ documentation/users_guide/usersguide.doc.hh
@@ -4,14 +4,15 @@
  * <ul>
  * <li>Introduction
  *     <ul>
- *     <li>Automated planning
- *     <li>EUROPA2 goals  
+ *     <li>@ref automatedplanning "Automated planning"
+ *     <li>@ref europa2introduction "EUROPA2"  
  *     <li>Users' Guide overview 
  *     </ul>
  * <li>Tutorial
  *     <ul>
+ *     <li> Constraint Based Interval Planning.
  *     <li>Hello world example
- *     <li>Plantary Rover Example
+ *     <li>Plantary rover Example
  *       <ul>
  *          <li>Domain modelling in NDDL
  *          <li>Planning with PLASMA
@@ -20,8 +21,9 @@
  *     </ul>
  * <li>Reference
  *     <ul>
- *       <li> System architecture
+ *       <li> System architecture       
  *       <li> CSP Primer
+ *       <li> Constraint Based Interval Planning.
  *       <li> NDDL Reference Manaual
  *       <li> Solver Reference Manaual
  *       <li> Plan Database Reference Manaual
