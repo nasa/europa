987a3c635025dd56a46df3e71708b0c04bd3b9c8
Peter Jarvis <no_author@no_author.com>
2005-10-21 15:35:29 +0000

    moved out from deprecated
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@3321 23e1bc29-d954-0410-9766-47de34158ba0

---
+++ documentation/users_guide/references.doc.hh
@@ -0,0 +1,6 @@
+/**
+ * @page references References
+ * @li @anchor Dechter91 Dechter, R; Meiri, I.; Pearl, J. "Temporal Constraint Networks". Artificial Intelligence 49(1):
+ * 69-95, 1991.
+ * @li @anchor Allen91 James Allen.
+ */
