c288c0f75041fda9f8454d0d1b1ed6102e81c7b5
Peter Jarvis <no_author@no_author.com>
2005-10-21 15:33:20 +0000

    added in glossary
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@3319 23e1bc29-d954-0410-9766-47de34158ba0

--- documentation/users_guide/background.doc.hh
+++ documentation/users_guide/background.doc.hh
@@ -1,5 +1,5 @@
 /**
- * @page background Background
+ * @page background Advanced Background Concepts
  * In this chapter we review some of the core concepts employed in the @ref europa "EUROPA" 2 Constraint-based Planning 
  * Paradigm. The sections are:
  * @li @ref csp
