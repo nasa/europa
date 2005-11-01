d9f817c61eb17674a43bb99824738a1dd90571fc
Peter Jarvis <no_author@no_author.com>
2005-11-01 17:39:01 +0000

    added documentation
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@3376 23e1bc29-d954-0410-9766-47de34158ba0

--- TemporalNetwork/component/TimepointWrapper.hh
+++ TemporalNetwork/component/TimepointWrapper.hh
@@ -7,16 +7,45 @@
 
 namespace EUROPA {
 
-  /**
-   * @brief Wraps a timepoint to manage deallocation in a staged manner that can be
-   * used to synchronize the propagator state.
-   */
+   /**
+     * @class  TimepointWrapper
+     * @brief Wraps a timepoint to manage deallocation in a staged manner that can be
+     *        used to synchronize the propagator state.
+     * @ingroup TemporalNetwork
+    */
+
   class TimepointWrapper : public Entity {
   public:
+    
+    /**
+     * @brief constructor 
+     * @param prop Temporal propagator managing this timepoint
+     * @param tempVar 
+     * @param point
+     */
     TimepointWrapper(const TemporalPropagatorId& prop, const ConstrainedVariableId& tempVar, const TimepointId& point);
+ 
+    /**
+     * @brief deconstructor
+     */
     virtual ~TimepointWrapper();
+    
+    /**
+     * @brief accessor for unique id of this TimepointWrapper
+     * @return unique id
+     */
     inline const EntityId& getId() { return m_id; }
+
+    /**
+     * @brief accessor for wrapped timepoint
+     * @return timepoint
+     */
     inline const TimepointId& getTimepoint() const {return m_timepoint;}
+
+    /**
+     * @brief accessor for constrained variable
+     * @return constrained variable's id
+     */
     inline const ConstrainedVariableId& getTempVar() const {return m_tempVar;}
   private:
     void handleDiscard();
