32bb0d703d13aca6844e12a31a7b2cad187f33c1
Peter Jarvis <no_author@no_author.com>
2005-11-08 18:58:11 +0000

    plandatabase and resource tests work together
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@3401 23e1bc29-d954-0410-9766-47de34158ba0

--- System/test/test-main.cc
+++ System/test/test-main.cc
@@ -4,8 +4,8 @@
 //#include "../../HeuristicsEngine/test/test-module.cc"
 //#include "../../HSTS/test/test-module.cc"
 //#include "../../NDDL/test/test-module.cc"
-//#include "../../PlanDatabase/test/test-module.cc"
-//#include "../../Resource/test/test-module.cc"
+#include "../../PlanDatabase/test/test-module.cc"
+#include "../../Resource/test/test-module.cc"
 //#include "../../RulesEngine/test/test-module.cc"
 //#include "../../Solvers/test/test-module.cc"
 //#include "../../TemporalNetwork/test/test-module.cc"
@@ -17,14 +17,19 @@
  */
 
 int main(int argc, const char** argv) {
+  // setup singleton entities for all tests.
+  SchemaId schema = Schema::instance();
+
   //AverModuleTests::runTests();  // looks like a path problem in the test itself when called from outside standard dir.
   //CBPlannerModuleTests::runTests(); // crashes.
   //ConstraintEngineModuleTests::runTests();  // load problem
   //HeuristicsEngineModuleTests::runTests(); // xmutils error
   //HSTSModuleTests::runTests(); // cannot find hstsnobranch...
   // NDDLModuleTets::runTests(); // conot find domain.hh
-  //PlanDatabaseModuleTests::runTests(); // runs on its own
-  //ResourceModuleTests::runTests(); // runs on its own
+
+  PlanDatabaseModuleTests::runTests();
+  ResourceModuleTests::runTests();
+
   // RulesEngineModuleTests::runTests(); // test rule not found
   //SolverModuleTests::runTests(); // won't ru n
   //TemporalNetworkModuleTests::runTests(); // cannot find testSubgoalRule
