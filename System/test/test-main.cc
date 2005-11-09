732c35b5a27110419df4b27a3c1579a05733c212
Peter Jarvis <no_author@no_author.com>
2005-11-09 04:52:20 +0000

    almost all includes working
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@3412 23e1bc29-d954-0410-9766-47de34158ba0

--- System/test/test-main.cc
+++ System/test/test-main.cc
@@ -1,15 +1,15 @@
-//#include "../../Aver/test/test-module.cc"
-//#include "../../CBPlanner/test/test-module.cc"
-//#include "../../ConstraintEngine/test/test-module.cc"
-//#include "../../HeuristicsEngine/test/test-module.cc"
-//#include "../../HSTS/test/test-module.cc"
-//#include "../../NDDL/test/test-module.cc"
+#include "../../Aver/test/test-module.cc"
+#include "../../CBPlanner/test/test-module.cc"
+#include "../../ConstraintEngine/test/test-module.cc"
+#include "../../HeuristicsEngine/test/test-module.cc"
 #include "../../PlanDatabase/test/test-module.cc"
 #include "../../Resource/test/test-module.cc"
-//#include "../../RulesEngine/test/test-module.cc"
+#include "../../RulesEngine/test/test-module.cc"
 //#include "../../Solvers/test/test-module.cc"
-//#include "../../TemporalNetwork/test/test-module.cc"
-//#include "../../Utils/test/test-module.cc"
+#include "../../TemporalNetwork/test/test-module.cc"
+#include "../../Utils/test/test-module.cc"
+#include "../../NDDL/test/test-module.cc"
+//#include "../../HSTS/test/test-module.cc"
 
 /**
  * @brief Test harness for running all module tests from a single binary to enable code coverage
@@ -21,16 +21,14 @@ int main(int argc, const char** argv) {
   SchemaId schema = Schema::instance();
 
   //AverModuleTests::runTests();  // looks like a path problem in the test itself when called from outside standard dir.
-  //CBPlannerModuleTests::runTests(); // crashes.
+  //CBPlannerModuleTests::runTests();
   //ConstraintEngineModuleTests::runTests();  // load problem
-  //HeuristicsEngineModuleTests::runTests(); // xmutils error
+  HeuristicsEngineModuleTests::runTests(); // xmutils error
   //HSTSModuleTests::runTests(); // cannot find hstsnobranch...
   // NDDLModuleTets::runTests(); // conot find domain.hh
-
   PlanDatabaseModuleTests::runTests();
-  ResourceModuleTests::runTests();
-
-  // RulesEngineModuleTests::runTests(); // test rule not found
+  //ResourceModuleTests::runTests();
+  //RulesEngineModuleTests::runTests(); // test rule not found
   //SolverModuleTests::runTests(); // won't ru n
   //TemporalNetworkModuleTests::runTests(); // cannot find testSubgoalRule
   //UtilModuleTests::runTests(); // test appears to fail
