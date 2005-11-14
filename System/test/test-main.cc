0498767b820018cf31d5b4ad1d55507964b3c234
Peter Jarvis <no_author@no_author.com>
2005-11-14 21:59:36 +0000

    tidy state - all includes working. 7 module tests working. 5 module tests to get working
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@3465 23e1bc29-d954-0410-9766-47de34158ba0

--- System/test/test-main.cc
+++ System/test/test-main.cc
@@ -1,38 +1,37 @@
-//#include "../../Solvers/test/solvers-test-module.hh" // three mulitple symbol defintion problems.
-//#include "../../NDDL/test/nddl-test-module.hh"
-//#include "../HSTS/test/hsts-test-module.hh"
-//#include "../../Utils/test/util-test-module.hh"
-#include "../Aver/test/aver-test-module.hh"
-#include "../CBPlanner/test/cbp-test-module.hh"
-#include "../ConstraintEngine/test/ce-test-module.hh"
-#include "../HeuristicsEngine/test/he-test-module.hh"
-#include "../PlanDatabase/test/db-test-module.hh"
-#include "../Resource/test/rs-test-module.hh"
-#include "../RulesEngine/test/re-test-module.hh"
-#include "../TemporalNetwork/test/tn-test-module.hh"
+#include "../../NDDL/test/nddl-test-module.hh"
+#include "../../HSTS/test/hsts-test-module.hh"
+#include "../../utils/test/util-test-module.hh"
+#include "../../Solvers/test/solvers-test-module.hh"
+#include "../../Aver/test/aver-test-module.hh"
+#include "../../CBPlanner/test/cbp-test-module.hh"
+#include "../../ConstraintEngine/test/ce-test-module.hh"
+#include "../../HeuristicsEngine/test/he-test-module.hh"
+#include "../../PlanDatabase/test/db-test-module.hh"
+#include "../../Resource/test/rs-test-module.hh"
+#include "../../RulesEngine/test/re-test-module.hh"
+#include "../../TemporalNetwork/test/tn-test-module.hh"
 
 /**
  * @brief Test harness for running all module tests from a single binary to enable code coverage
  *        analysis tools to examine the codebase.
  */
 
+// stubbed out - just provide a defintion of this symbol.
 extern "C" void loadSchema() {}
 
 
 int main(int argc, const char** argv) {
- 
-  //SolversModuleTests::runTests("../../Solvers/test");
-  //NDDLModuleTets::runTests("../../NDDL/test");
-  //UtilModuleTests::runTests("../../Utils/test");
   //HSTSModuleTests::runTests("../../hsts/test");
   AverModuleTests::runTests("../../Aver/test");
   CBPlannerModuleTests::runTests("../../CBPlanner/test");
-  //ConstraintEngineModuleTests::runTests("../../ConstraintEngine/test"); // assertion failure in domian-tests.cc
+  //ConstraintEngineModuleTests::runTests("../../ConstraintEngine/test"); // assertion failure in domian-tests.cc a is b   instance of a can be compared to instance of b
+  //SolversModuleTests::runTests("../../Solvers/test");  // Factory "A" is not registered.
   PlanDatabaseModuleTests::runTests("../../PlanDatabase/test");
   ResourceModuleTests::runTests("../../Resource/test");
   RulesEngineModuleTests::runTests("../../RulesEngine/test");
   HeuristicsEngineModuleTests::runTests("../../HeuristicsEngine/test");
   TemporalNetworkModuleTests::runTests("../../TemporalNetwork/test"); 
-
+  //UtilModuleTests::runTests("../../Utils/test");
+  //NDDLModuleTests::runTests("../../NDDL/test");
   return 0;
 }
