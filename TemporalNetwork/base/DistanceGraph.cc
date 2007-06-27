4e94ff508e5c09bb6cd16e1c3fd65fe4d2ee1820
No Author <no_author@no_author.com>
2007-06-27 16:30:08 +0000

    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/tags/MSLICE_G1_RC1@4380 23e1bc29-d954-0410-9766-47de34158ba0

--- TemporalNetwork/base/DistanceGraph.cc
+++ TemporalNetwork/base/DistanceGraph.cc
@@ -99,12 +99,16 @@ DedgeId DistanceGraph::findEdge(DnodeId from, DnodeId to)
   // Cache node vars -- Chucko 22 Apr 2002 
   Int fromOutCount = from->outCount; 
   if (fromOutCount > 0) {
+    /*
     DedgeId* fromOutArray = from->outArray;
     for (Int i=0; i < fromOutCount; i++) {
       DedgeId edge = fromOutArray[i];
       if (to == edge->to)
 	return edge;
     }
+    */
+    // PHM 06/20/2007 Speedup by using map instead.
+    return from->edgemap[to];
   }
   return DedgeId::noId();
 }
@@ -161,9 +165,10 @@ DedgeId DistanceGraph::createEdge(DnodeId from, DnodeId to, Time length)
   edge->from = from;
   edge->to = to;
   edge->length = length;
-  this->edges.push_back(edge);
+  this->edges.insert(edge);
   attachEdge (from->outArray, from->outArraySize, from->outCount, edge);
   attachEdge (to->inArray, to->inArraySize, to->inCount, edge);
+  from->edgemap[to] = edge;
   return edge;
 }
 
@@ -171,12 +176,14 @@ Void DistanceGraph::deleteEdge(DedgeId edge)
 {
   detachEdge (edge->from->outArray, edge->from->outCount, edge);
   detachEdge (edge->to->inArray, edge->to->inCount, edge);
+  edge->from->edgemap.erase(edge->to);
   eraseEdge(edge);
 }
 
 Void DistanceGraph::eraseEdge(DedgeId edge)
 {
-  deleteIfEqual(edges, edge);
+  //deleteIfEqual(edges, edge);
+  edges.erase(edge);
   edge->from = DnodeId::noId();
   edge->to = DnodeId::noId();
   edge->length = 99;  // A clue for debugging purposes
@@ -344,7 +351,15 @@ Bool DistanceGraph::incBellmanFord()
 Void DistanceGraph::dijkstra (DnodeId source, DnodeId destination)
 {
  check_error(isValid(source), "node is not defined in this graph");
- check_error(isValid(destination), "node is not defined in this graph");
+
+ // PHM 05/16/2007 The previous isValid(destination) check was
+ // mistaken; a null destination was intended to be allowed; in that
+ // case dijkstra computes the distance to ALL nodes in the graph.
+ // (See DistanceGraph.hh, which has destination = noId() as default!)
+
+ check_error(destination.isNoId() || isValid(destination),
+             "node is not null or defined in this graph");
+
  //debugMsg("DistanceGraph:dijkstra", "from " << source << " to " << destination);
   source->distance = 0;
   source->depth=0;
@@ -606,7 +621,7 @@ bool DistanceGraph::hasNode(const DnodeId node) const {
 std::string DistanceGraph::toString() const {
  std::stringstream sstr;
 
- for (std::vector<DedgeId>::const_iterator it = edges.begin(); it != edges.end(); ++it){
+ for (std::set<DedgeId>::const_iterator it = edges.begin(); it != edges.end(); ++it){
    DedgeId edge = *it;
    sstr << edge->from << " " << edge->to << " " << edge->length << std::endl;
  }
