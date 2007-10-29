73a573791733040016b88f3342d959ecf18a5253
Tristan Smith <tristan.b.smith@nasa.gov>
2007-10-29 19:49:44 +0000

    Doxygen now generates code documentation but nothing else.  I've deleted stuff that is already on the wiki and moved other things to the deprecated directory (someone should spend time looking through that directory for useful nuggets
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@4481 23e1bc29-d954-0410-9766-47de34158ba0

--- documentation/main.doc.hh
+++ documentation/main.doc.hh
@@ -1,4 +1,4 @@
 /**
- * @mainpage EUROPA
- * EUROPA documentation has been moved to the <a href="https://babelfish.arc.nasa.gov/trac/europa/wiki"> EUROPA wiki page <\a>. This reference remains only for providing Doxygen documentation of the code for those interested in that view of the project.
+ * @mainpage
+ * EUROPA documentation has been moved to the <a href="https://babelfish.arc.nasa.gov/trac/europa/wiki"> EUROPA wiki page </a>. This reference remains only for providing Doxygen documentation of the code for those interested in that view of the project.
  */
