f630c23f2428d20879da4cbefe4768929620c157
Javier Barreiro <javier.barreiro@gmail.com>
2007-10-30 23:45:50 +0000

    modified Main.cc used by makefile to work with StandardAssembly and PSEngine
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@4508 23e1bc29-d954-0410-9766-47de34158ba0

--- System/component/Main.cc
+++ System/component/Main.cc
@@ -2,28 +2,48 @@
  * @file Main.cc
  *
  * @brief Provides an executable for your project which will use a
- * standard Chronological backtracking planner and a Test Assembly of
- * EUROPA
+ * standard Chronological backtracking planner and a StandardAssembly or a PSEngine
+ * to encapsulate EUROPA
  */
 
 #include "Nddl.hh" /*!< Includes protypes required to load a model */
 #include "SolverAssemblyWithResources.hh" /*!< For using a test EUROPA Assembly */
+#include "PSEngine.hh" 
+#include "Debug.hh"
 
 using namespace EUROPA;
 
-/**
- * @brief Uses the planner to solve a planning problem
- */
-int main(int argc, const char ** argv){
+void executeWithAssembly(const char* plannerConfig, const char* txSource);
+bool executeWithPSEngine(const char* plannerConfig, const char* txSource, int startHorizon, int endHorizon, int maxSteps);
+void printFlaws(int it, PSList<std::string>& flaws);
+
+int main(int argc, const char ** argv)
+{
   if (argc != 3) {
     std::cerr << "Must provide initial transactions file." << std::endl;
     return -1;
   }
 
   const char* txSource = argv[1];
-
   const char* plannerConfig = argv[2];
+  
+  executeWithAssembly(plannerConfig,txSource);
+  
+  /*
+  executeWithPSEngine(
+      plannerConfig,
+      txSource,
+      0,   // startHorizon
+      100, // endHorizon
+      1000 // maxSteps
+  ); 
+  */
+     
+  return 0;
+}
 
+void executeWithAssembly(const char* plannerConfig, const char* txSource)
+{
   // Initialize Library  
   SolverAssemblyWithResources::initialize();
 
@@ -48,3 +68,62 @@ int main(int argc, const char ** argv){
 
   std::cout << "Finished\n";
 }
+
+bool executeWithPSEngine(const char* plannerConfig, const char* txSource, int startHorizon, int endHorizon, int maxSteps)
+{
+    try {
+	  PSEngine engine;
+	
+	  engine.start();
+	  engine.executeTxns(txSource,true,true);
+	
+	  PSSolver* solver = engine.createSolver(plannerConfig);
+	  solver->configure(startHorizon,endHorizon);
+	  int i;
+      for (i = 0; 
+           !solver->isExhausted() &&
+           !solver->isTimedOut() &&
+           i<maxSteps; i = solver->getStepCount()) {
+		  solver->step();
+		  PSList<std::string> flaws;
+		  if (solver->isConstraintConsistent()) {
+	          flaws = solver->getFlaws();
+        	  printFlaws(i,flaws);
+			  if (flaws.size() == 0)
+			      break;
+		  }
+		  else
+			debugMsg("Main","Iteration " << i << " Solver is not constraint consistent");
+	  }
+	  
+	  if (solver->isExhausted()) {
+	      debugMsg("Main","Solver was exhausted after " << i << " steps");	  
+	  }
+	  else if (solver->isTimedOut()) {
+	      debugMsg("Main","Solver timed out after " << i << " steps");
+	  }
+	  else {    
+	      debugMsg("Main","Solver finished after " << i << " steps");
+	  }	      
+	      
+	  delete solver;	
+	  engine.shutdown();
+
+	  return true;
+	}
+	catch (Error& e) {
+		std::cerr << "PSEngine failed:" << e.getMsg() << std::endl;
+		return false;
+	}	
+}
+
+void printFlaws(int it, PSList<std::string>& flaws)
+{
+	debugMsg("Main","Iteration:" << it << " " << flaws.size() << " flaws");
+	
+	for (int i=0; i<flaws.size(); i++) {
+		std::cout << "    " << (i+1) << " - " << flaws.get(i) << std::endl;
+	}
+}
+
+
