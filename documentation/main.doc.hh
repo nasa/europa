577b54c80edc4207e1b3b9b490a6c13d85c237a6
Tristan Smith <tristan.b.smith@nasa.gov>
2008-01-04 19:00:14 +0000

    Unnecessary and complicates multiple doxygen pieces
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@4701 23e1bc29-d954-0410-9766-47de34158ba0

--- documentation/main.doc.hh
+++
@@ -1,4 +0,0 @@
-/**
- * @mainpage
- * EUROPA documentation has been moved to the <a href="https://babelfish.arc.nasa.gov/trac/europa/wiki"> EUROPA wiki page </a>. This reference remains only for providing Doxygen documentation of the code for those interested in that view of the project.
- */
