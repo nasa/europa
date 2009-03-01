518d7b69d8dec922e19ba9342cf36f79a5a7715d
Mark Roberts <mak.c.roberts@gmail.com>
2009-03-01 07:17:06 +0000

    Cleaned up subdirectories for log4cpp.
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@5247 23e1bc29-d954-0410-9766-47de34158ba0

--- bin/.makeproject/platforms.mk
+++ bin/.makeproject/platforms.mk
@@ -96,6 +96,7 @@ LOADLIBS += -lSystem$(BUILD_SUFFIX) \
             -lPlanDatabase$(BUILD_SUFFIX) \
             -lConstraintEngine$(BUILD_SUFFIX) \
             -lUtils$(BUILD_SUFFIX) \
-            -lTinyXml$(BUILD_SUFFIX)
+            -lTinyXml$(BUILD_SUFFIX) \
+            -lLog4cpp$(BUILD_SUFFIX) 
 
 vpath %.dylib $(EUROPA_HOME)/lib
