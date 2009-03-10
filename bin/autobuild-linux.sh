4f4b5d637df05a7861c1829a4445b6427297b9fc
Mark Roberts <mak.c.roberts@gmail.com>
2009-03-10 16:17:06 +0000

    An autobuild script for linux that runs all builds and saves output.  This should eventually be made into a python script that runs on any platform.
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@5271 23e1bc29-d954-0410-9766-47de34158ba0

---
+++ bin/autobuild-linux.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+##Author: Mark Roberts
+##runs all possible builds and places output into files based on the build name.
+## TODO PROFILE, SHARED is not actually in the official autobuild
+
+libraries=(  \
+    SHARED \
+    STATIC
+)
+
+variants=(  \
+#    DEV \
+#    OPTIMIZED \
+    PROFILE
+)
+
+for variant in ${variants[@]}
+do
+    for library in ${libraries[@]}
+    do
+	echo Working on  $variant $library
+	rm build.$variant.$library.log
+	touch build.$variant.$library.log
+	xterm -title "Working on $variant.$library" -geometry 144x24 -e tail -f build.$variant.$library.log &
+	ant -Djam.variant=$variant -Djam.libraries=$library autobuild > build.$variant.$library.log 2>&1
+    done
+done
+
