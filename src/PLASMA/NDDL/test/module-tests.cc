3292c4758d5a67793671a701f04367c1239349a3
Mark Roberts <mak.c.roberts@gmail.com>
2009-06-10 04:36:46 +0000

    Changes to support 64bit compile.  Not sure if these changes will work on other platforms too.
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@5443 23e1bc29-d954-0410-9766-47de34158ba0

--- src/PLASMA/NDDL/test/module-tests.cc
+++ src/PLASMA/NDDL/test/module-tests.cc
@@ -1,7 +1,9 @@
 #include "nddl-test-module.hh"
+#include <stdlib.h>
 
 #include <cppunit/ui/text/TestRunner.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
+
 
 CPPUNIT_TEST_SUITE_REGISTRATION( NDDLModuleTests );
 
@@ -11,7 +13,7 @@ int main( int argc, char **argv)
   CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
   runner.addTest( registry.makeTest() );
 
-  const char* baselineVar=std::getenv("SET_TEST_BASELINE");
+  const char* baselineVar=getenv("SET_TEST_BASELINE");
   bool setBaseline = (baselineVar!=NULL && (std::string(baselineVar)=="1"));
   runner.addTest(new ErrorCheckingTests("ErrorCheckingTests.txt",setBaseline));
   return !runner.run("", false);
