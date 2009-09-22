d5fbf406b38baa4d4cba11cf17b3d338b7cd0c42
Javier Barreiro <javier.barreiro@gmail.com>
2009-09-22 07:05:39 +0000

    added virtual destructor to avoid warning
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@5793 23e1bc29-d954-0410-9766-47de34158ba0

--- src/PLASMA/Utils/base/LoggerInterface.hh
+++ src/PLASMA/Utils/base/LoggerInterface.hh
@@ -3,9 +3,9 @@
 
 #include <iostream>
 
-namespace EUROPA 
+namespace EUROPA
 {
-	
+
 	enum EuropaDebugInt { //copied from log4cxx
 		OFF_INT = 1000000,
 		FATAL_INT = 50000,
@@ -18,9 +18,9 @@ namespace EUROPA
 	};
 
 	/**
- 	* Provides an abstraction layer for levels so that other classes can implement 
+ 	* Provides an abstraction layer for levels so that other classes can implement
  	* their own level ints (for example, log4cxx or log4cpp).
- 	*/   
+ 	*/
 	enum LoggerLevel {
 		ALL = ALL_INT,
 		TRACE = TRACE_INT,
@@ -28,28 +28,30 @@ namespace EUROPA
 		INFO = INFO_INT,
 		WARN = WARN_INT,
 		ERROR_LVL = ERROR_INT,  //ERROR is a windows macro, so we added "_LVL" to work around this problem
-		FATAL = FATAL_INT, 
+		FATAL = FATAL_INT,
 		UNDEFINED = FATAL_INT + 10000
 	};
 
-	
+
 	#define EUROPA_LOGGER_ROOT_DEFAULT_NAME "rootLogger"
 	#define EUROPA_LOGGER_ROOT_DEFAULT_LEVEL DEBUG
 
 	class LoggerInterface
 	{
 	public:
+	    virtual ~LoggerInterface() {}
+
 		virtual void setLevel( LoggerLevel level ) = 0;
 		virtual LoggerLevel getLevel( void ) const = 0;
 		virtual bool levelEnabled( LoggerLevel level ) const = 0;
 		virtual std::string getCategory( void ) const = 0;
-		
+
 		virtual bool operator<( const LoggerInterface &right ) const = 0;
 
 		//helper methods
 		virtual std::string loggerLevelToString( LoggerLevel level ) = 0;
 		virtual LoggerLevel stringToLoggerLevel( std::string in ) = 0;
-		
+
 		//for function-based logging
 		virtual std::ostream &log( LoggerLevel level, std::string msg ) = 0;
 		virtual std::ostream &alwaysLog( std::string msg ) = 0;
