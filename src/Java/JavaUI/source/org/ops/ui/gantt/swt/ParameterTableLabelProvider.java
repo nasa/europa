ee8ec89d1a013b194fdca82333ecfb42a3425e78
Tristan Smith <tristan.b.smith@nasa.gov>
2011-06-20 22:38:59 +0000

    Handle enumerations in Details View.
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@6336 23e1bc29-d954-0410-9766-47de34158ba0

--- src/Java/JavaUI/source/org/ops/ui/gantt/swt/ParameterTableLabelProvider.java
+++ src/Java/JavaUI/source/org/ops/ui/gantt/swt/ParameterTableLabelProvider.java
@@ -4,6 +4,7 @@ import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.swt.graphics.Image;
 
+import psengine.PSValueList;
 import psengine.PSVariable;
 
 /**
@@ -41,13 +42,41 @@ public class ParameterTableLabelProvider extends ColumnLabelProvider {
 				return variable.getEntityName();
 			}
 			else {
+				
+				
 				if(variable.isSingleton()) {
 					return variable.getSingletonValue().toString();
 				}
-				else {
+				
+				else if (variable.isInterval()) {
 					// TODO:  Combine this with gantt hover functionality for a common output
 					// format for variables:
 					return "[" + variable.getLowerBound() + ", " + variable.getUpperBound() + "]";
+				}
+				else if (variable.isEnumerated()){ 
+					String retval = "{";
+					
+					PSValueList vals = variable.getValues();
+					
+					if(vals.size() == 1) {
+						return vals.get(0).toString();
+					}
+					
+					boolean firstOne = true;
+					for(int i = 0 ;i < vals.size(); ++i) {
+						if(!firstOne) {
+							retval = retval + ", " + vals.get(i).toString();
+						}
+						else {
+							retval = "{" + vals.get(i).toString();
+							firstOne = false;
+						}
+					}
+					
+					return retval + "}";
+				}
+				else {
+					return "TODO:  Provide labels for variable " + variable.getEntityName();
 				}
 			}
 		}
