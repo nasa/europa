8ed1bd5bb6193c8adcc6b9db42bcbc4e06626a7a
Javier Barreiro <javier.barreiro@gmail.com>
2011-08-30 00:35:34 +0000

    in JavaUI: updated AstNodeTypes to match the latest NDDL3.tokens
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@6370 23e1bc29-d954-0410-9766-47de34158ba0

--- src/Java/JavaUI/source/org/ops/ui/filemanager/model/AstNodeTypes.java
+++ src/Java/JavaUI/source/org/ops/ui/filemanager/model/AstNodeTypes.java
@@ -9,15 +9,15 @@ public interface AstNodeTypes {
 	public static final int NDDL = 8;
 	public static final int IDENT = 16;
 	public static final int CLASS_DEF = 48; // 'class'
-	public static final int PREDICATE_DEF = 51; // '::'
+	public static final int PREDICATE_DEF = 52; // '::'
 	public static final int LBRACE = 33; // '{'
 	public static final int VARIABLE = 11;
 	public static final int CONSTRUCTOR = 5;
 	public static final int LPAREN = 41; // '('
 	// mention of predicate in class
 	public static final int PREDICATE_KEYWORD = 50; // 'predicate'
-	public static final int GOAL_KEYWORD = 57;
-	public static final int FACT_KEYWORD = 58;
+	public static final int GOAL_KEYWORD = 58;
+	public static final int FACT_KEYWORD = 59;
 	// instantiation of predicate
 	public static final int PREDICATE_INSTANCE = 9;
 	public static final int CONSTRAINT_INSTANCE = 4;	
