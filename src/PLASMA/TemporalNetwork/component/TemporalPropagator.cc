32369a695b7b149dc15d183894e714adb17015e2
Paul Morris <Paul.H.Morris@nasa.gov>
2011-10-12 15:58:53 +0000

    Restored efficiency improvement, now passing test
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@6400 23e1bc29-d954-0410-9766-47de34158ba0

--- src/PLASMA/TemporalNetwork/component/TemporalPropagator.cc
+++ src/PLASMA/TemporalNetwork/component/TemporalPropagator.cc
@@ -834,8 +834,8 @@ namespace EUROPA {
     static unsigned int sl_counter(0);
     sl_counter++;
 
-    Time lb = lbc; //mapToInternalInfinity(lbc);
-    Time ub = ubc; //mapToInternalInfinity(ubc);
+    Time lb = mapToInternalInfinity(lbc);
+    Time ub = mapToInternalInfinity(ubc);
 
     if(tnetConstraint.isNoId())
       return m_tnet->addTemporalConstraint(m_tnet->getOrigin(), getTimepoint(var), lb, ub);
