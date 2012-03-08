1358201a7d020d89ac9d26fc4059c84cbb09a474
Javier Barreiro <javier.barreiro@gmail.com>
2012-03-08 22:26:51 +0000

    checkpoint. adding constraint engine interfaces.
    git-svn-id: https://europa-pso.googlecode.com/svn/PLASMA/trunk@6535 23e1bc29-d954-0410-9766-47de34158ba0

--- src/Java/Europa-J/src/org/europa/ce/ConstraintEngine.java
+++ src/Java/Europa-J/src/org/europa/ce/ConstraintEngine.java
@@ -1,5 +1,41 @@
 package org.europa.ce;
 
-public interface ConstraintEngine {
+import java.util.List;
+import java.util.Set;
 
+import org.europa.engine.EngineComponent;
+
+public interface ConstraintEngine 
+	extends EngineComponent
+{
+	public CVariable createVariable(
+		String name,
+		String dataType,
+		Domain baseDomain,
+		Object parent,
+		int index,
+		boolean isInternal,
+		boolean canBeSpecified);
+	
+	public CVariable getVariableByKey(long key);
+	public Set<CVariable> getAllVariables();
+	
+	public Constraint createConstraint(
+		String constraintType,
+		List<CVariable> scope,
+		String violationExpl);
+
+	public Constraint getConstraintByKey(long key);
+	public void deleteConstraint(Constraint c);
+	public Set<Constraint> getAllConstraints();
+	
+	public double getViolation();
+	public List<String> getViolationExpl();
+	public Set<Constraint> getViolatedConstraints();
+	
+	public void propagate();
+	public boolean getAutopropagate();
+	public void setAutopropagate(boolean b);
+
+	public CESchema getSchema();
 }
