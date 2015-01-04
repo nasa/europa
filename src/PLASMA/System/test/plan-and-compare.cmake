0af87bf6c57d3451b948f5dba40483276ed046ea
Michael Iatauro <michael.j.iatauro@nasa.gov>
2015-01-04 15:40:52 -0800

    Re-adding plan comparison, currently only using diff

---
+++ src/PLASMA/System/test/plan-and-compare.cmake
@@ -0,0 +1,16 @@
+execute_process(COMMAND ${exec_plan} ${model} ${configFile} ${language}
+  OUTPUT_FILE ${output_file}
+  ERROR_FILE ${output_file}
+  RESULT_VARIABLE result)
+if(NOT ${result} EQUAL 0)
+  message(FATAL_ERROR ${result})
+endif(NOT ${result} EQUAL 0)
+
+if(plan_compare)
+  execute_process(COMMAND ${plan_compare} ${gold_file} ${output_file}
+    OUTPUT_FILE ${diff_file}
+    RESULT_VARIABLE diff_result)
+  if(NOT ${diff_result} EQUAL 0)
+    message(FATAL_ERROR ${diff_result})
+  endif(NOT ${diff_result} EQUAL 0)
+endif(plan_compare)
